dc3b88bb0ec8ee952b2c28f2d6f8f32159c622f5
CH <1925480119@qq.com>
2020-09-04 15:31:29 +0800

    [Chenhui] fix : pix accessing property test

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -5,6 +5,12 @@ export default function countCharacter(string, prediction) {
   //
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
+  if (string === '' || string === null || string === undefined) {
+    return 0;
+  }
 
-  throw new Error('Please delete this line and implement the function');
+  if (string.length && prediction === undefined) {
+    return string.length;
+  }
+
 }
