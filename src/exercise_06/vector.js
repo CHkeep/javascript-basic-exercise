2e444935433cfa168bd63da749a915b1e11a613d
CH <1925480119@qq.com>
2020-09-05 23:52:55 +0800

    [Chenhui] fix: pix accesingproperty test

--- src/exercise_06/vector.js
+++ src/exercise_06/vector.js
@@ -13,4 +13,32 @@ export default class Vector {
   // class.
 
   // PLEASE DELETE THIS LINE AND ADD YOUR IMPLEMENTATION HERE
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    Object.defineProperty(this, 'x', { writable: false });
+    Object.defineProperty(this, 'y', { writable: false });
+  }
+
+
+  static plus(vector1, vector2) {
+    const x = vector1.x + vector2.x;
+    const y = vector1.y + vector2.y;
+    // eslint-disable-next-line no-undef
+    return new Vector(x, y);
+  }
+
+  static minus(vector1, vector2) {
+    const x = vector1.x - vector2.x;
+    const y = vector1.y - vector2.y;
+    // eslint-disable-next-line no-undef
+    return new Vector(x, y);
+  }
+
+  distance() {
+    // eslint-disable-next-line no-undef
+    const dis = Math.hypot(this.x, this.y);
+    // eslint-disable-next-line no-undef
+    return dis;
+  }
 }
