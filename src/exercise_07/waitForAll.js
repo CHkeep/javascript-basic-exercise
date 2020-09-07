38a35feeb18555d41feeab85766e7c3d27b40744
CH <1925480119@qq.com>
2020-09-07 11:23:29 +0800

    [Chenhui] fix: submit exercise_07

--- src/exercise_07/waitForAll.js
+++ src/exercise_07/waitForAll.js
@@ -9,5 +9,40 @@ export default function waitForAll(...promises) {
   // * Please implement this function and pass all the tests in wait_for_all_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < promises.length; i++) {
+    if (!(promises[i] instanceof Promise)) {
+      throw new Error('Not all elements are promises.');
+    };
+  }
+
+  return new Promise((resolve, reject) => {
+    const len = promises.length;
+    let rejectCount = 0;
+    // eslint-disable-next-line no-shadow
+    let counter = 0;
+    const complete = () => {
+      if (rejectCount > 0) {
+        reject();
+      } else {
+        resolve();
+      }
+    };
+    // eslint-disable-next-line no-shadow
+    const count = () => {
+      counter += 1;
+      if (counter >= len) {
+        complete();
+      }
+    };
+    for (let i = 0; i < len; i += 1) {
+      promises[i].then(() => {
+        count();
+      // eslint-disable-next-line no-loop-func
+      }, () => {
+        rejectCount += 1;
+        count();
+      });
+    }
+  });
 }
